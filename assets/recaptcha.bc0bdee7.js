e6a894ca4a6122d59553190a87d2cb57d1726d04
mypollapp <appmypoll@gmail.com>
2025-08-09 11:20:42 +0200

    Add new options

---
+++ assets/recaptcha.bc0bdee7.js
@@ -0,0 +1,1 @@
+import{b as e}from"./index.d798d0f4.js";import{V as o}from"./ReCaptchaVuePlugin.542b8737.js";var p=e(({app:a})=>{a.use(o,{siteKey:"6Lf0aJIrAAAAAGVtZ4U3plREo2bDHG2pwk54MO66",loaderOptions:{autoHideBadge:!1}})});export{p as default};
---
+++ assets/render.0d331bc7.js
@@ -0,0 +1,1 @@
+import{aq as c,bq as a}from"./index.d798d0f4.js";function v(n,r){return n!==void 0&&n()||r}function h(n,r){if(n!==void 0){const i=n();if(i!=null)return i.slice()}return r}function s(n,r){return n!==void 0?r.concat(n()):r}function d(n,r,i,o,t,u){r.key=o+t;const e=c(n,r,i);return t===!0?a(e,u()):e}export{h as a,d as b,s as c,v as h};
