d56e6c168229c165e7f46bdffce39a79c359287c
mypollapp <appmypoll@gmail.com>
2025-08-23 23:56:47 +0200

    Add parameter to the random api

---
+++ assets/recaptcha.7fd6001b.js
@@ -0,0 +1,1 @@
+import{b as e}from"./index.f3930e0c.js";import{V as o}from"./ReCaptchaVuePlugin.890bf005.js";var p=e(({app:a})=>{a.use(o,{siteKey:"6Lf0aJIrAAAAAGVtZ4U3plREo2bDHG2pwk54MO66",loaderOptions:{autoHideBadge:!1}})});export{p as default};
