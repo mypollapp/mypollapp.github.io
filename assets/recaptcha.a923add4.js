eeb46fe02969d23faa4a5e69540da346398c861e
mypollapp <appmypoll@gmail.com>
2025-08-24 16:20:53 +0200

    Set the right API_BASE_URL

---
+++ assets/recaptcha.a923add4.js
@@ -0,0 +1,1 @@
+import{b as e}from"./index.e7908ff7.js";import{V as o}from"./ReCaptchaVuePlugin.56fa8c76.js";var p=e(({app:a})=>{a.use(o,{siteKey:"6Lf0aJIrAAAAAGVtZ4U3plREo2bDHG2pwk54MO66",loaderOptions:{autoHideBadge:!1}})});export{p as default};
