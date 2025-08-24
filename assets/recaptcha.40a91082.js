b8ddbc6c3e75e4facbc0847fd2d5ea018b7320d8
mypollapp <appmypoll@gmail.com>
2025-08-24 16:11:05 +0200

    Add new polls and pie chart

---
+++ assets/recaptcha.40a91082.js
@@ -0,0 +1,1 @@
+import{b as e}from"./index.7c322237.js";import{V as o}from"./ReCaptchaVuePlugin.0bc390f5.js";var p=e(({app:a})=>{a.use(o,{siteKey:"6Lf0aJIrAAAAAGVtZ4U3plREo2bDHG2pwk54MO66",loaderOptions:{autoHideBadge:!1}})});export{p as default};
