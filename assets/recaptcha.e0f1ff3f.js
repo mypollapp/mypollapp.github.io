cc964b8baed2afc5eaa3114243f0129bd98683d5
mypollapp <appmypoll@gmail.com>
2025-10-05 18:42:57 +0200

    Consulta Pacto Histórico

---
+++ assets/recaptcha.e0f1ff3f.js
@@ -0,0 +1,1 @@
+import{b as e}from"./index.4479dcf5.js";import{V as o}from"./ReCaptchaVuePlugin.46acd107.js";var p=e(({app:a})=>{a.use(o,{siteKey:"6Lf0aJIrAAAAAGVtZ4U3plREo2bDHG2pwk54MO66",loaderOptions:{autoHideBadge:!1}})});export{p as default};
